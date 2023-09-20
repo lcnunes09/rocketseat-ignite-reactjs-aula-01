b99254ee3eceb6c86ce9b54db05307c8f09780a2
Luciana Nunes <lcnunes09@gmail.com>
2023-09-20 19:43:12 -0400

    feat: add new comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
+import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }){
+    const [comments, setComments] = useState([
+        'Muito bom!',
+        'Excelente postagem!',
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -14,6 +25,17 @@ export function Post({ author, publishedAt, content }){
         locale: ptBR,
         addSuffix: true
     })
+            
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
+    }
 
     return (
         <article className={styles.post}>
@@ -31,23 +53,26 @@ export function Post({ author, publishedAt, content }){
             </header>
 
             <div className={styles.content}>
-                {content.map((item, index) => {
+                {content.map(item => {
                     switch(item.type) {
                         case 'paragraph':
-                            return <p key={index}>{item.content}</p>
+                            return <p key={item.content}>{item.content}</p>
                         case 'link':
-                            return <a key={index} href="#">{item.content}</a>
+                            return <p key={item.content}><a href="#">{item.content}</a></p>
                         default:
                             return null
                     }
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>                
 
                 <textarea 
+                    name="comment"
                     placeholder="Escreva seu comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type='submit'>Enviar</button>
@@ -55,10 +80,11 @@ export function Post({ author, publishedAt, content }){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => (
+                    <Comment key={comment} content={comment} />
+                ))}
             </div>
         </article>
-    )
+    )        
 }
+
